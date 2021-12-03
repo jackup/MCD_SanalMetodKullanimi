7a30231d84e23cdb120596d6ce9f6c8832c102d8
jackup <yk.58@hotmail.com>
2021-12-03 17:05:17 +0300

    Add project files.

---
+++ MCD_SanalMetodKullanimi/Base.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_SanalMetodKullanimi
+{
+    public class Base
+    {
+        public Base()
+        {
+            Console.WriteLine("Base Class");
+        }
+
+        public virtual void EkranaYaz(string data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
---
+++ MCD_SanalMetodKullanimi/Urun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_SanalMetodKullanimi
+{
+    public class Urun : Base
+    {
+        public Urun()
+        {
+            Console.WriteLine("Ürün");
+        }
+
+        public override void EkranaYaz(string data)
+        {
+            Console.WriteLine("Ürün => " + data);
+        }
+    }
+}
